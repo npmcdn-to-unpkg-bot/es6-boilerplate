3be93fc4c0e3c49d452bbf168053482fa64706b8
DDO <dav@icarys.org>
2016-04-14 17:19:32 +0200

    added few things

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,69 +1,23 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache');
-var minifycss = require('gulp-minify-css');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+import gulp from 'gulp';
+import clean from 'gulp-clean';
+import taskLisinting from 'gulp-task-listing';
 
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-       baseDir: "./"
-    }
-  });
+let buildDest = 'dist';
+let clientSrc = './src/client/';
+let serverSrc = './src/server/';
+
+gulp.task('default', taskLisinting ); 
+      
+
+gulp.task('clean', () => {
+    return gulp.src( buildDest, {read: false})
+    .pipe(clean());
 });
 
-gulp.task('bs-reload', function () {
-  browserSync.reload();
-});
+gulp.task('build', () => {
+    console.log('To be implemented')
+})
 
-gulp.task('images', function(){
-  gulp.src('src/client/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/images/'));
-});
-
-gulp.task('styles', function(){
-  gulp.src(['src/client/styles/**/*.scss'])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('dist/styles/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('dist/styles/'))
-    .pipe(browserSync.reload({stream:true}))
-});
-
-gulp.task('scripts', function(){
-  return gulp.src('src/client/scripts/**/*.js')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/scripts/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts/'))
-    .pipe(browserSync.reload({stream:true}))
-});
-
-gulp.task('default', ['browser-sync'], function(){
-  gulp.watch("src/client/styles/**/*.scss", ['styles']);
-  gulp.watch("src/client/scripts/**/*.js", ['scripts']);
-  gulp.watch("*.html", ['bs-reload']);
-});
+gulp.task('serve', () => {
+    console.log('To be implemented')
+})
