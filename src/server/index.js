b1f90aaecdf1d8cd67b7dde76536d7c0cce99bb5
icarys29 <dav@icarys.org>
2016-04-15 01:23:53 +0200

    gulp browserSync

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,18 @@
 console.log("Starting server")
+// Importing node modules
+import express from 'express';
+// Importing source files
+import routes from './routes/main.routes';
+// consts
+const app = express();
 
+app.use('/', routes);
 
+// arrow functions
+const server = app.listen(3000, () => {
+	// destructuring
+  const {address, port} = server.address();
 
-// ES6 test
-let app = "coucou";
-
-export default app;
+  // string interpolation:
+  console.log(`Example app listening at http://${address}:${port}`);
+});
